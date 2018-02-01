3cdb09bfd13872b0985c204d2fdd55c65ae607aa
mkalmheiri <mkalmheiri@mail.csuchico.edu>
2018-02-01 11:53:44 -0800

    Describe what is new in your own words

--- madlibs.cpp
+++ madlibs.cpp
@@ -1,9 +1,27 @@
 //Authors: 
 #include <iostream>
 #include <string>
+using namespace std;
+
 
 int main()
 {
+string name; 
+int x;
+double y;
 
-  return 0;
+
+
+
+
+cout<< "Enter an animal name" << std::endl;
+cin>> name;
+cout<< "Enter a number" <<std::endl;
+cin>>x;
+cout<< "Enter a decimal" <<std::endl;
+cin>>y;
+
+cout<< "This morning I went to my "<< name << " I bought this "<< name << " for " <<x <<" $ and "<< y <<" $ cent."<<endl;
+
+return 0;
 }
